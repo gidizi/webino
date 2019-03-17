99b98d5d206c0a6138d307ecbcffa2ea8ee0ab43
gidi zimmer <gidizimmer@gmail.com>
2019-03-17 13:58:26 +0200

    using dj_database_url

--- Aido/settings.py
+++ Aido/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,6 +91,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600) #for prod
+DATABASES['default'].update(db_from_env) #for prod
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
