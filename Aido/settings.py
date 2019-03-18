44b337a2217b9517738af88ece5e3e7c18b465a2
gidi zimmer <gidizimmer@gmail.com>
2019-03-18 15:35:22 +0200

    going back to debug mode - handling media files requiers external server such as s3 bucket, apache or nigx and i want to avoid it for now

--- Aido/settings.py
+++ Aido/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'au*=h!sm6-pr69rln6%tw7a^l&d6%2qm3i8@&#i!alw^81jz0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'webinoapp.herokuapp.com']
 
